c3322f41848fac433637e8e199aea2d2083a1be6
Function Store <47873029+function-store@users.noreply.github.com>
2024-10-06 04:11:35 +0200

    added CustomParTools to panes too (to be removed from toolbar)

--- scripts/QuickParent/QuickParentExt.py
+++ scripts/QuickParent/QuickParentExt.py
@@ -5,23 +5,22 @@ class QuickParentExt:
 		self.ownerComp = ownerComp
 		CustomParHelper.Init(self, ownerComp, enable_properties=True, enable_callbacks=True)
 		self.popDialog = self.ownerComp.op('popDialog')
+		self.paneParent = None
 
-
-	@property
-	def paneParent(self):
-		return ui.panes.current.owner
 	
 
-	def AddParentshortcut(self):
-		defaultText = self.paneParent.name if self.paneParent.par.parentshortcut.eval() is None else self.paneParent.par.parentshortcut.eval()
+	def AddParentshortcut(self, _target):
+		self.paneParent = _target
+		defaultText = self.paneParent.name if self.paneParent.par.parentshortcut.eval() == '' else self.paneParent.par.parentshortcut.eval()
 		self.popDialog.Open(text='Add Parent Shortcut', title='Add Parent Shortcut', buttons=['OK', 'Cancel'],
 			escButton=2, enterButton=1, escOnClickAway=True, textEntry=defaultText, callback=self.OnAddParentshortcutCallback)
 	
 
 	def OnAddParentshortcutCallback(self, result):
 		if result['button'] == 'OK':
-			parentshortcutpar = self.paneParent.par.parentshortcut
-			if parentshortcutpar is not None:
-				parentshortcutpar.val = result['enteredText']
+			if self.paneParent is not None:
+				parentshortcutpar = self.paneParent.par.parentshortcut
+				if parentshortcutpar is not None:
+					parentshortcutpar.val = result['enteredText']
 
 
