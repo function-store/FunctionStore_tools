78e0fe22e94476deb4a8bd5616eba7debee1af0b
Function Store <47873029+function-store@users.noreply.github.com>
2024-10-06 04:52:16 +0200

    added keyboard shortcut callbacks

---
+++ scripts/QuickExt/template/extKeyboardin_callbacks.py
@@ -0,0 +1,35 @@
+# me - This DAT
+# 
+# dat - The DAT that received the key event
+# keyInfo - A namedtuple containing the following members:
+#	key - The name of the key attached to the event.
+#			This tries to be consistent regardless of which language
+#			the keyboard is set to. The values will be the english/ASCII
+#			values that most closely match the key pressed.
+#			This is what should be used for shortcuts instead of 'character'.
+#	webCode - The name of the key following web-programming standards.
+#	character - The unicode character generated.
+#	alt - True if the alt modifier is pressed
+#	lAlt - True if the left-alt modifier is pressed
+#	rAlt - True if the right-alt modifier is pressed
+#	ctrl - True if the ctrl modifier is pressed
+#	lCtrl - True if the left-ctrl modifier is pressed
+#	rCtrl - True if the right-ctrl modifier is pressed
+#	shift - True if the shift modifier is pressed
+#	lShift - True if the left-shift modifier is pressed
+#	rShift - True if the right-shift modifier is pressed
+#	state - True if the event is a key press event
+#	time - The time when the event came in milliseconds
+#	cmd - True if the cmd modifier is pressed
+#	lCmd - True if the left-cmd modifier is pressed
+#	rCmd - True if the right-cmd modifier is pressed
+
+def onKey(dat, keyInfo):
+	return
+
+# shortcutName is the name of the shortcut
+
+def onShortcut(dat, shortcutName, time):
+	mod.extUtils.CustomParHelper.OnKeyboardShortcut(shortcutName)
+	return;
+	
