0e9f0d033ae923b407cb96fc10d5f9a5a324c50a
Function Store <47873029+function-store@users.noreply.github.com>
2024-10-10 04:18:06 +0200

    removed unused code

--- scripts/QuickExt/templates/ExtUtils/NoNode/extDatSizeChangeExec.py
+++ scripts/QuickExt/templates/ExtUtils/NoNode/extDatSizeChangeExec.py
@@ -1,27 +1,3 @@
-# me - this DAT.
-# 
-# dat - the changed DAT
-# rows - a list of row indices
-# cols - a list of column indices
-# cells - the list of cells that have changed content
-# prev - the list of previous string contents of the changed cells
-# 
-# Make sure the corresponding toggle is enabled in the DAT Execute DAT.
-# 
-# If rows or columns are deleted, sizeChange will be called instead of row/col/cellChange.
-
-
-def onTableChange(dat):
-	return
-
-def onRowChange(dat, rows):
-	return
-
-def onColChange(dat, cols):
-	return
-
-def onCellChange(dat, cells, prev):
-	return
 
 def onSizeChange(dat):
 	package = mod(me.dock.name)
--- scripts/QuickExt/templates/ExtUtils/NoNode/extDatTableChangeExec.py
+++ scripts/QuickExt/templates/ExtUtils/NoNode/extDatTableChangeExec.py
@@ -1,30 +1,6 @@
-# me - this DAT.
-# 
-# dat - the changed DAT
-# rows - a list of row indices
-# cols - a list of column indices
-# cells - the list of cells that have changed content
-# prev - the list of previous string contents of the changed cells
-# 
-# Make sure the corresponding toggle is enabled in the DAT Execute DAT.
-# 
-# If rows or columns are deleted, sizeChange will be called instead of row/col/cellChange.
-
 
 def onTableChange(dat):
 	package = mod(me.dock.name)
 	package.NoNode.OnDatExec(package.DatExecType.TableChange, dat)
 	return
 
-def onRowChange(dat, rows):
-	return
-
-def onColChange(dat, cols):
-	return
-
-def onCellChange(dat, cells, prev):
-	return
-
-def onSizeChange(dat):
-	return
-	
