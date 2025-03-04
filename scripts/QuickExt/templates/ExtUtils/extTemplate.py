fb3e2a00ce34070f56e984440a0c6c678b1cf8b9
Function Store <47873029+function-store@users.noreply.github.com>
2025-03-04 22:55:53 +0100

    removed NoNode from templates due to some permanence issues
    There's an annoying "bug?" when you rename or move your COMP that uses NoNode features, that you need to toggle cooking of the comp for path references to update on some dependables

--- scripts/QuickExt/templates/ExtUtils/extTemplate.py
+++ scripts/QuickExt/templates/ExtUtils/extTemplate.py
@@ -1,14 +1,12 @@
 # < - DO NOT REMOVE THIS VERY IMPORTANT LINE!!! used by QuickExt to inject extension - >
 
 CustomParHelper: CustomParHelper = next(d for d in me.docked if 'ExtUtils' in d.tags).mod('CustomParHelper').CustomParHelper # import
-NoNode: NoNode = next(d for d in me.docked if 'ExtUtils' in d.tags).mod('NoNode').NoNode # import
 ###
 
 class QuickExtTemplate:
 	def __init__(self, ownerComp):
 		self.ownerComp = ownerComp
 		CustomParHelper.Init(self, ownerComp, enable_properties=True, enable_callbacks=True)
-		NoNode.Init(ownerComp, enable_chopexec=True, enable_datexec=True, enable_parexec=True, enable_keyboard_shortcuts=True)
 
 	
 
