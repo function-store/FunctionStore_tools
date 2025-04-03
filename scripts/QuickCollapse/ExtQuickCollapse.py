c5921ac8a861a15d2b9267b42ddf04a862ebd516
Function Store <47873029+function-store@users.noreply.github.com>
2025-04-03 07:26:28 +0200

    fixed QuickCollapse esc

--- scripts/QuickCollapse/ExtQuickCollapse.py
+++ scripts/QuickCollapse/ExtQuickCollapse.py
@@ -7,45 +7,48 @@ class ExtQuickCollapse:
 		CustomParHelper.Init(self, ownerComp, enable_properties=True, enable_callbacks=True)
 		self.popDialog = ownerComp.op('popDialog')
 		self.newCOMP = None
+		self.selected = None
 
 	def undoCollapse(self, isUndo, _return):
 		ui.panes.current.owner = _return
 		pass
 	
 	def OnCollapse(self, customize=False):
-		selected = ui.panes.current.owner.selectedChildren
-		if not selected:
+		self.selected = ui.panes.current.owner.selectedChildren
+		if not self.selected:
 			return
-		
+
+		if customize:
+			self.popDialog.Open(callback=self.OnCustomizeCallback)
+			return
+		else:
+			self.collapse()
+		pass
+
+	def collapse(self, _name=None, _shortcut=None):
 		ui.undo.startBlock('Collapsing')
 		ui.undo.addCallback(self.undoCollapse, info = ui.panes.current.owner)
-
-		selected[0].parent().collapseSelected()
-		
+		self.selected[0].parent().collapseSelected()
 		self.newCOMP = ui.panes.current.owner.currentChild
 
 		if not self.newCOMP:
-			return
-		
-		if customize:
-			self.popDialog.Open(callback=self.OnCustomizeCallback)
 			ui.undo.endBlock()
 			return
-
-		ui.panes.current.owner = self.newCOMP
-		ui.undo.endBlock()
-		pass
-
-	def OnCustomizeCallback(self, info):
-		_name = info['enteredText'][0]
-		_shortcut = info['enteredText'][1]
-
+		
 		if _name:
 			self.newCOMP.name = _name
 		if _shortcut:
 			self.newCOMP.par.parentshortcut = _shortcut
 
 		ui.panes.current.owner = self.newCOMP
+		ui.undo.endBlock()
+
+
+
+	def OnCustomizeCallback(self, info):
+		if info['buttonNum'] == 1:
+			self.collapse(_name=info['enteredText'][0], _shortcut=info['enteredText'][1])
+			
 
 	
 
