fe047bc85ec1be79aefa24984dcbc5b446d7e173
Function Store <47873029+function-store@users.noreply.github.com>
2025-04-05 06:52:27 +0200

    generalized colors UI
    TODO: externalize to file, save/load

--- scripts/ColorUI/ExtColorUI.py
+++ scripts/ColorUI/ExtColorUI.py
@@ -16,23 +16,140 @@ class ExtColorUI:
 	def __init__(self, ownerComp):
 		self.ownerComp = ownerComp
 		CustomParHelper.Init(self, ownerComp, enable_properties=True, enable_callbacks=True)
+		
+		# before anything happens
+		self.activeColorTable : tableDAT= self.ownerComp.op('null_active')
+		self.default_fam_colors = {}
+
 		self.table_out : tableDAT = self.ownerComp.op('table_out')
 		self.colors_saved = {}
+		self.colors_changed = {}
+		self.logger = self.ownerComp.op('Logger').ext.Logger
 
-	def __randomizeColors(self):
-		for ui_element in ui.colors:
-			self._randomizeColor(ui_element)
-		pass
 
-	def _randomizeColor(self, ui_element):
-		ui.colors[ui_element] = [random.uniform(0, 1) for _ in range(3)] 
+	def OnStart(self):
+		for _row in self.activeColorTable.rows(val=True):
+			self.default_fam_colors[_row[0]] = _row[1:]
+		self.logger.log(f'Default families colors: {self.default_fam_colors}')
+		if self.evalAutoloadfamiliescolors:
+			self.onParLoadfamiliescolors()
 
-	def onParResetcolors(self):
+	def onParResetallcolors(self):
 		ui.colors.resetToDefaults()
-		self.parRandomize.val = False
+		self.colors_changed = {}
+		self.ownerComp.store('colors', {})
+
+
+	def onParUielement(self, val):
+		# when selecting ui element from dropdown or by typing in the text field
+		if val in ui.colors:
+			self.ownerComp.parGroup.Color = ui.colors[val]
+#
+	def onParSetcolor(self):
+		# set ui element to a color
+		try:
+			if self.evalUielement not in self.colors_saved:
+				self.colors_saved[self.evalUielement] = ui.colors[self.evalUielement]
+			if self.setColor(self.evalUielement, self.ownerComp.parGroup.Color.eval()):
+				pass
+		except:
+			pass
+
+	def onParResetcolor(self):
+		try:
+			if self.setColor(self.evalUielement, self.colors_saved[self.evalUielement]):
+				self.ownerComp.parGroup.Color = self.colors_saved[self.evalUielement]
+				self.colors_changed.pop(self.evalUielement)
+		except:
+			pass
+
+	def onParSaveallcolors(self):
+		_colors : dict[str, list[float]] = self.ownerComp.fetch('colors', {})
+		self.logger.log(f'Saving colors: {self.colors_changed}')
+		for _ui_element in self.colors_changed:
+			_colors[_ui_element] = self.colors_changed[_ui_element]
+
+		self.ownerComp.store('colors', _colors)
+
+	def onParLoadallcolors(self):
+		_colors : dict[str, list[float]] = self.ownerComp.fetch('colors', {})
+		self.colors_changed = {}
+		for _ui_element in _colors:
+			self.setColor(_ui_element, _colors[_ui_element])
+
+###
+	@property
+	def __available_families(self):
+		return self.activeColorTable.col(0, val=True)
+
+	@property
+	def sequence(self) -> Sequence:
+		return self.ownerComp.seq.Family
+
+	def populateSequence(self):
+		families = self.__available_families
+		
+		self.sequence.numBlocks = 1
+		self.sequence.numBlocks = len(families)
+		for block in self.sequence.blocks:
+			fam = families[block.index]
+			# for some reason setting the MenuSource with TableMenu kept evaluating correctly but throwing an error
+			block.par.Type.menuNames = families
+			block.par.Type.menuLabels = families
+			
+			block.par.Type = fam
+			block.parGroup.Rgb.val = self.activeColorTable.row(fam)[1:]
+
+
+	def setColor(self, _type: str, _color: list[float]):
+		if _type in ui.colors:
+			ui.colors[_type] = _color
+			self.colors_changed[_type] = _color
+			return True
+		return False
+
+
+	def onParSavefamiliescolors(self):
+		_just_fams_colors : dict[str, list[float]] = self.ownerComp.fetch('fam_colors', {})
+		_colors : dict[str, list[float]] = self.ownerComp.fetch('colors', {})
+		
+		for _row in self.activeColorTable.rows(val=True):
+			_just_fams_colors[_row[0]] = _row[1:]
+			_colors[_row[0]] = _row[1:]
+
+		self.ownerComp.store('fam_colors', _just_fams_colors)
+		self.ownerComp.store('colors', _colors)
+
+
+	def onParLoadfamiliescolors(self):
+		_just_fams_colors : dict[str, list[float]] = self.ownerComp.fetch('fam_colors', self.default_fam_colors)
+		if _just_fams_colors:
+			for _fam, _color in _just_fams_colors.items():
+				if _fam in ui.colors:
+					ui.colors[_fam] = _color
+				else:
+					# TODO: handle custom OP families
+					pass
+		self.populateSequence()
+
+	def onParSetfamiliescolors(self):
+		for block in self.sequence.blocks:
+			block : SequenceBlock = block
+			self.setColor(block.par.Type.eval(), block.parGroup.Rgb.eval())
+
+	def onParResetfamiliescolors(self):
+		self.logger.log(f'Resetting families colors to defaults: {self.default_fam_colors}')
+		for _fam in self.default_fam_colors:
+			self.setColor(_fam, self.default_fam_colors[_fam])
+		self.ownerComp.store('fam_colors', {})
+		self.populateSequence()
+
+####
 
 	def onParRandomize(self):
-		self.__randomizeColors()
+		for ui_element in ui.colors:
+			self.setColor(ui_element, [random.uniform(0, 1) for _ in range(3)])
+		pass
 
 
 	def _whichUIElement(self, color):
@@ -52,26 +169,3 @@ class ExtColorUI:
 	def onParCheck(self):
 		colortocheck = self.evalGroupColortocheck
 		self._whichUIElement(colortocheck)
-
-
-	def onParUielement(self, val):
-		if val in ui.colors:
-			self.ownerComp.parGroup.Color = ui.colors[val]
-#
-	def onParSetcolor(self):
-		# store current color
-		try:
-			if self.evalUielement not in self.colors_saved:
-				self.colors_saved[self.evalUielement] = ui.colors[self.evalUielement]
-			# set new color
-			ui.colors[self.evalUielement] = self.ownerComp.parGroup.Color.eval()
-		except:
-			pass
-
-	def onParResetcolor(self):
-		try:
-			ui.colors[self.evalUielement] = self.colors_saved[self.evalUielement]
-			self.ownerComp.parGroup.Color = self.colors_saved[self.evalUielement]
-		except:
-			pass
-
